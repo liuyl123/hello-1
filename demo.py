7de9d86b4c953e7e4809500ca672e01a0adb0222
larus <larusx@163.com>
2014-12-01 22:52:34 +0800

    demo

--- demo.py
+++ demo.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request
+# coding=utf-8
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
-
+app.Debug = True
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'GET':
         return render_template('index.html')
-    else:
-
+    elif request.method == 'POST':
+        Date = request.form['Date']
+        Consume = request.form['Consume']
+        ret = dict()
+        ret['Date'] = Date
+        ret['Consume'] = Consume
+        return json.dumps(ret)
 
 
 if __name__ == '__main__':
